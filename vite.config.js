856b51c985b41e5f39f58a3d89e5bc7f7219b70b
KingKaglu <giorgigzirishvili2004l@gmail.com>
2025-06-14 00:29:30 +0400

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ command, mode }) => {
   return {
-    base: command === 'serve' ? '/' : '/jeweleryStore/', // Serve locally with base '/'
+    base: command === '/', 
     plugins: [react()],
   };
 });
